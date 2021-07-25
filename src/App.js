edf494d80644645523c0c318279e952df29f4c16
khanhduy160397 <khanhduy160397@gmail.com>
2021-07-25 21:56:23 +0700

    update css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
 import "../src/App.css";
 import humidity from "./assets/humidity.png";
-import sunny from "./assets/sunny.gif";
+import sunny from "./assets/sunny.png";
 import temp from "./assets/cels.png";
 import uvIndex from "./assets/uv.png";
 import wID from "./assets/wind.png";
-import rain from "./assets/rainny.gif";
-import cloud from "./assets/cloudy.gif";
-import night from "./assets/night.gif"
+import rain from "./assets/rain.png";
+import cloud from "./assets/mostly_cloud.png";
+import night from "./assets/cloud-night.PNG"
 import axios from "../node_modules/axios"
-import night_sky from "./assets/dark-sky.jpg"
+import night_sky from "./assets/night.jpg"
 import moon from "./assets/moon.png";
 import sunny_sky from "./assets/sunny.png"
-import sky_sun from "./assets/sunny_sky.jpg"
+import sky_sun from "./assets/morning_sky.jpg"
 
 function App() {
   const [date, setDate] = useState("");
@@ -29,59 +29,59 @@ function App() {
 
   // Call api to get value
   useEffect(async () => {
-      
-    const response = await axios.get(
-      "https://api.weatherapi.com/v1/current.json?key=2b5ae3f80e774339b5461932212204&q=Ho Chi Minh&aqi=yes"
-    );
-    console.log(response.data);
-    const data_current = response.data.current;
-    const data_location = response.data.location;
-    setCels(data_current.temp_c);
-    setHumid(data_current.humidity);
-    setWind(data_current.wind_mph);
-    setDate(data_current.last_updated);
-    setLocalTime(data_location.localtime);
-    setLocaltion(data_location.name);
-    if(data_current.is_day !== 1) {
-      setIsDay(false);
-    }
-
-    if(isDay) {
-      
-      if (data_current.condition.text.includes("rain")) {
-        setWeather(rain);
-        setBgWeather(sky_sun);
-      } else if (data_current.condition.text.includes("cloud")) {
-        setWeather(cloud);
-        setBgWeather(sky_sun);
-      } else {
-        setWeather(sunny_sky);
-        setBgWeather(sky_sun);
-      }
-      
-    } else {
-      setWeather(moon);
-      setBgWeather(night_sky);
-    }
-
-    if(document.getElementById('weather_div') != null) {
-      if(isDay) {
-        document.getElementById('weather_div').style.textShadow = "0px 0px 6px #797979";
-        document.querySelector('.weather_item').classList.add("weather_item_sun");
-        document.querySelector('.weather_content_left').classList.remove('text-white');
-      } else {
-        document.querySelectorAll('.weather_content_infor').forEach(item=>{
-          item.classList.add('text_color_white');
-        })
-      }
-    }
     
-    setLoading(false);
-  }, [loading, weather, background_weather, isDay]);
+    async function getData() {
 
-  function handleChange(event) {
-    event.target.value = 0 ? setIsDay(true) : setIsDay(false)
-  }
+      const response = await axios.get(
+        "https://api.weatherapi.com/v1/current.json?key=037d9da07cb84156aea151026212605&q=Ho Chi Minh&aqi=yes"
+      );
+      console.log(response.data);
+      const data_current = response.data.current;
+      const data_location = response.data.location;
+      setCels(data_current.temp_c);
+      setHumid(data_current.humidity);
+      setWind(data_current.wind_mph);
+      setDate(data_current.last_updated);
+      setLocalTime(data_location.localtime);
+      setLocaltion(data_location.name);
+      if(data_current.is_day !== 1) {
+        setIsDay(false);
+      }
+
+      if(isDay) {
+        
+        if (data_current.condition.text.includes("rain")) {
+          setWeather(rain);
+          setBgWeather(sky_sun);
+        } else if (data_current.condition.text.includes("cloud")) {
+          setWeather(cloud);
+          setBgWeather(sky_sun);
+        } else {
+          setWeather(sunny_sky);
+          setBgWeather(sky_sun);
+        }
+        
+      } else {
+        setWeather(moon);
+        setBgWeather(night_sky);
+      }
+  
+      if(document.getElementById('weather_div') != null) {
+        if(isDay) {
+          document.getElementById('weather_div').style.textShadow = "0px 0px 6px #797979";
+          document.querySelector('.weather_item').classList.add("weather_item_sun");
+          document.querySelector('.weather_content_left').classList.remove('text-white');
+        } else {
+          document.querySelectorAll('.weather_content_infor').forEach(item=>{
+            item.classList.add('text_color_white');
+          })
+        }
+      }
+      
+      setLoading(false);
+    }
+    getData();
+  }, [loading, weather, background_weather, isDay]);
 
   if (loading) {
     return <>
@@ -94,25 +94,21 @@ function App() {
   } else {
     return (
       <>
-        <div className="container-fluid weather_section px-0" id="weather_div">
-        <select onChange={handleChange}>
-            <option value={0}>Day</option>
-            <option value={1}>Night</option>
-          </select>
+        <div className="container weather_section px-0" id="weather_div">
             <div className="weather_content">
                 <img src={background_weather} width="100%" height="100%" />
                 <div className="mx-auto weather_item">
-                    <div className="weather_content_left container text-white d-flex align-items-center justify-content-center">
+                    <div className="weather_content_left py-3 container text-white d-flex align-items-center justify-content-center">
                         <div className="weather_location">
-                           <h1 className="weather_location_title">
+                           <p className="weather_location_title">
                              {location}
-                           </h1>
+                           </p>
                            <h3 className="local_time">
                              {localTime}
                            </h3>
-                           <h1 className="temp">
+                           <p className="temp">
                               {cels} <sup>o</sup> C
-                            </h1>
+                            </p>
                         </div>
                     </div>
                     <div className="weather_content_right d-flex align-items-center">
